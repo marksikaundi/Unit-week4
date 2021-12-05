e2b5fd3b65cbba4db090b5a8c0ea8a3c311a6aa0
marksikaundi <emmanuelmark117@gmail.com>
2021-12-05 19:55:25 +0200

    codes updated

---
+++ Reading.py
@@ -0,0 +1,32 @@
+def add(num1, num2): # for adding 
+    return num1 + num2
+
+def sub(num3, num4):
+    return num3 - num4
+
+def mult(num5, num6):
+    return num5 * num6
+
+def divi(num7, num8):
+    return num7 / num8
+    
+    
+    
+
+def main():
+    myResults = add(20, 20)
+    print(myResults)
+
+    mySample = sub(20, 20)
+    print(mySample)
+
+    myValue = mult(20, 20)
+    print(myValue)
+
+    myDivision = divi(20, 20)
+    print(myDivision)
+
+main()
+    
+
+    
