4a1e293186d72f0fa122d4892d3514b2131f0ea1
marksikaundi <emmanuelmark117@gmail.com>
2021-12-06 23:52:32 +0200

    discusson forum

---
+++ discusson.py
@@ -0,0 +1,14 @@
+
+def factorial(n):
+    space = ' ' * (4 * n)
+    print(space, 'factorial', n)
+    if n == 0:
+        print(space, 'returning 1')
+        return 1
+    else:
+        recurse = factorial(n-1)
+        result = n * recurse
+        print(space, 'returning', result)
+        return result
+
+print(factorial(7))
