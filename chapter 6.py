4e9ce657bc87e94b2f1dffb0a80b2d91f389ca19
marksikaundi <emmanuelmark117@gmail.com>
2021-12-06 06:34:56 +0200

    cleaned

--- chapter 6.py
+++ chapter 6.py
@@ -45,3 +45,23 @@ def factorial(n): # recursive function
         recurse = factorial(n-1)
         result = n * recurse
         return result
+
+def fibonacci(n): # leap faith function
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def factorial(n):
+    if not isinstance(n, int):
+        print('Factorial is only defined for integers.')
+        return None
+    elif n < 0:
+        print('Factorial is not defined for negative integers.')
+        return None
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
