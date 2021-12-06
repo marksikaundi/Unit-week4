2b3f5518aeda6a97cebf0e812a96110e5a658d32
marksikaundi <emmanuelmark117@gmail.com>
2021-12-07 00:46:49 +0200

    t

---
+++ quiz.py
@@ -0,0 +1,74 @@
+n = 2
+n += 5
+n
+
+def area(l, w):
+    temp = l * w;
+    return temp
+
+l = 4.0
+w = 3.25
+x = area(l, w)
+if ( x ):
+  print (x)
+
+  #################################
+def function2(param):
+        print (param, param)
+        print (cat)
+
+def function1(part1, part2):
+    cat = part1 + part2
+    function2(cat)
+
+chant1 = "See You "
+chant2 = "See Me "
+function1(chant1, chant2)
+
+
+
+n = int(10.)
+
+print(isinstance(n, float), isinstance(n * 1.0, float))
+
+def function2(param):
+    print (param, param)
+
+def function1(part1, part2):
+    cat = part1 + part2
+    function2(cat)
+
+chant1 = "See You "
+chant2 = "See Me "
+function1(chant1, chant2)
+
+# def test_function( length, width, height):
+#     print ("the area of the box is ",length*width*height)
+#     return length*width*height
+
+# l = 12.5
+# w = 5
+# h = 2
+# test_function(l, w, h)
+
+
+def test_function( length, width, height):
+    print ("the area of the box is ", length*width*height)
+    return length*width*height
+
+l = 12.5
+w = 5
+h = 2
+test_function(l, w, h)
+
+print ("The area of the box is ", length*width*height)
+
+def area(l, w):
+    temp = l * w;
+    return temp
+
+l = 4.0
+w = 3.25
+x = area(l, w)
+if ( x ):
+  print (x)
